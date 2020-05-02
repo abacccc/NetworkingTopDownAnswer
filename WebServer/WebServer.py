000ce7bce326c19ab7d4dc5d3e37a4bc70755e62
abacccc <yanghao12688@yeah.net>
2020-05-02 21:59:08 +0800

    web server

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -1,1 +1,38 @@
-
+#import socket module
+from socket import *
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a sever socket
+#Fill in start
+serverPort = 12000
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+#Fill in end
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        #Send one HTTP header line into socket
+        #Fill in start
+        responseHeader = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nContent-Length: %d\n\n' % (len(outputdata))
+        connectionSocket.send(responseHeader.encode())
+        #Fill in end
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+    except IOError:
+        #Send response message for file not found
+        #Fill in start
+        failHeader = 'HTTP/1.1 404 Not Found\nConnection: close\n\n'
+        connectionSocket.send(failHeader.encode())
+        #Fill in end
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
+serverSocket.close()
